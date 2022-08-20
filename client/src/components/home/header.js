3f3912224b8fbd5794965b5a9deac0625b74fa43
puzzlehead <dylanlim12698@gmail.com>
2022-08-20 14:52:18 +0800

    Create base Header component

---
+++ client/src/components/home/header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { AppBar, Typography, Toolbar, Box, Button } from '@mui/material';
+
+const Header = () => {
+  const navItems = ['HOME', 'CREATE'];
+
+  return (
+    <AppBar component="nav">
+      <Toolbar>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+        >
+          aptSolve
+        </Typography>
+        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+          {navItems.map((item) => (
+            <Button key={item} sx={{ color: '#fff' }}>
+              {item}
+            </Button>
+          ))}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
