2829ba199ed1d75f27de9731c32325127ad3779f
puzzlehead <dylanlim12698@gmail.com>
2022-08-25 23:40:07 +0800

    Refactor code

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -4,7 +4,7 @@ import ProblemForm from './problemForm/problemForm';
 const Create = () => {
   return (
     <section className="max-w-4xl p-6 mx-auto bg-indigo-600 rounded-md shadow-md dark:bg-gray-800 mt-20">
-      <ProblemForm />
+      <ProblemForm textColor="text-white" isEdit={false} />
     </section>
   );
 };
