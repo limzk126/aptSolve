7b2bc6b40e68f9d2f9d9b4d4bb6b086f0d466c32
puzzlehead <dylanlim12698@gmail.com>
2022-08-27 11:46:37 +0800

    Implement middleware for error handling

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('express-async-errors');
 
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const problemRouter = require('./controllers/problem');
+const middleware = require('./utils/middleware');
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -22,5 +24,6 @@ app.get('/', (request, response) => {
   response.send('<h1>The server is running.</h1>');
 });
 app.use('/api/problems', problemRouter);
+app.use(middleware.errorHandler);
 
 module.exports = app;
---
+++ server/utils/middleware.js
@@ -0,0 +1,13 @@
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted values' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+module.exports = {
+  errorHandler,
+};
