b6b50a36053a06d35634a3b98ebe9f2c138a3699
puzzlehead <dylanlim12698@gmail.com>
2022-08-27 21:42:00 +0800

    Make api accept token as header

--- client/src/services/problem.js
+++ client/src/services/problem.js
@@ -1,28 +1,47 @@
 import axios from 'axios';
 const baseUrl = '/api/problems';
 
+let token;
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`;
+};
+
 const getAll = async () => {
-  const response = await axios.get(baseUrl);
+  const config = {
+    headers: { Authorization: token },
+  };
+  const response = await axios.get(baseUrl, config);
   console.log('response', response);
   return response.data;
 };
 
 const create = async (newProblem) => {
-  const response = await axios.post(baseUrl, newProblem);
+  const config = {
+    headers: { Authorization: token },
+  };
+  const response = await axios.post(baseUrl, newProblem, config);
   return response.data;
 };
 
 const update = async (modifiedProblem) => {
+  const config = {
+    headers: { Authorization: token },
+  };
   const response = await axios.put(
     `${baseUrl}/${modifiedProblem.id}`,
-    modifiedProblem
+    modifiedProblem,
+    config
   );
   return response.data;
 };
 
 const deleteProblem = async (id) => {
-  const response = await axios.delete(`${baseUrl}/${id}`);
+  const config = {
+    headers: { Authorization: token },
+  };
+  const response = await axios.delete(`${baseUrl}/${id}`, config);
   return response.data;
 };
 
-export default { getAll, create, update, deleteProblem };
+export default { setToken, getAll, create, update, deleteProblem };
